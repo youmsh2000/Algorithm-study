9feb90a3559d8a12e4ff235d243a9d57b74dfff9
염승혁 (알고리즘 스터디 순한맛) <165366131+youmsh2000@users.noreply.github.com>
2024-05-23 20:39:38 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int sum(int n)          //셀프넘버가 아닌 수를 구하는 함수
+{
+    int sum = n;
+
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+int main(void)
+{
+    int arr[10001], i, check;
+
+    for (i = 1; i < 10001; i++)
+    {
+        check = sum(i);
+        if (check < 10001)       //셀프 넘버가 아닌 수 확인
+            arr[check] = 1;
+    }
+
+    for (i = 1; i < 10001; i++)
+    {
+        if (arr[i] != 1)          //셀프 넘버 수 확인
+            printf("%d\n", i);
+    }
+    return 0;
+}
